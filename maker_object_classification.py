352b9d7423806dd9d56be5f16075573a6b1ab7d9
Ishaan Adhikary <IshaanAdhikary@outlook.com>
2026-02-21 18:06:19 -0500

    Basic CLIP CV and visualization set-up

---
+++ maker_object_classification.py
@@ -0,0 +1,36 @@
+import cv2
+from transformers import pipeline
+from ultralytics import YOLO
+from PIL import Image
+
+# Load models
+checkpoint = "openai/clip-vit-base-patch32"
+detector = pipeline(model=checkpoint, task="zero-shot-image-classification")
+
+labels = ["fork", "cup", "backpack", "phone"]
+labels = [f'This is a photo of {label}.' for label in labels]
+
+cap = cv2.VideoCapture(0) #Webcam
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("No camera feed. Exiting video...")
+        break
+
+    # CLIP pipeline to label object
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    image = Image.fromarray(rgb)
+    predictions = detector(image, labels)
+    
+    best = predictions[0]
+    label = best["label"]
+    score = best["score"]
+    frame = cv2.putText(frame, f"{label}: {score:.2f}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    # Window management
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
